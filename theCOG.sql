a8ec36494fd93df7dc212443b4937f635a6de26c
jumpegg <jumpegg@naver.com>
2017-06-23 15:33:09 +0900

    #3

--- theCOG.sql
+++ theCOG.sql
@@ -1,21 +1,21 @@
 CREATE TABLE `user` (
-  `id` varchar(200) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `nickname` varchar(200) NOT NULL,
-  `avatar` varchar(500) DEFAULT NULL,
-  `is_del` int(11) NOT NULL DEFAULT '0',
+  `id` varchar(200) NOT NULL, 						-- 아이디(PK)
+  `name` varchar(200) NOT NULL, 					-- 이름
+  `nickname` varchar(200) NOT NULL, 			-- 별칭
+  `avatar` varchar(500) DEFAULT NULL, 		-- 프로필 사진
+  `is_del` int(11) NOT NULL DEFAULT '0', 	-- 삭제유뮤
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `post` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(200) NOT NULL,
-  `content` text,
-  `img` varchar(500) DEFAULT NULL,
-  `tag` varchar(500) DEFAULT NULL,
-  `is_del` varchar(500) NOT NULL DEFAULT '0',
+  `id` int(11) NOT NULL AUTO_INCREMENT,				-- 인덱스(PK)
+  `user_id` varchar(200) NOT NULL,						-- user 외래키(FK)
+  `content` text,															-- post 내용
+  `img` varchar(500) DEFAULT NULL,						-- post 이미지
+  `tag` varchar(500) DEFAULT NULL,						-- 해시태그
+  `is_del` varchar(500) NOT NULL DEFAULT '0', -- 삭제유무
   `createdAt` datetime NOT NULL,
   `updateAt` datetime NOT NULL,
   PRIMARY KEY (`id`),
@@ -23,11 +23,11 @@ CREATE TABLE `post` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `comment` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(200) NOT NULL,
-  `post_id` int(11) NOT NULL,
-  `comment` text,
-  `is_del` int(11) NOT NULL DEFAULT '0',
+  `id` int(11) NOT NULL AUTO_INCREMENT,		-- 인덱스 (PK)
+  `user_id` varchar(200) NOT NULL,				-- user 외래키(FK)
+  `post_id` int(11) NOT NULL,							-- post 외래키(FK)
+  `comment` text,													-- comment 내용
+  `is_del` int(11) NOT NULL DEFAULT '0',	-- 삭제유무
   `createdAt` datetime NOT NULL,
   `updateAt` datetime NOT NULL,
   PRIMARY KEY (`id`),
@@ -36,9 +36,9 @@ CREATE TABLE `comment` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `like` (
-  `id` int(11) NOT NULL,
-  `user_id` varchar(200) NOT NULL,
-  `post_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,						-- 인덱스(PK)
+  `user_id` varchar(200) NOT NULL,	-- user 외래키(FK)
+  `post_id` int(11) NOT NULL,				-- post 외래키(FK)
   PRIMARY KEY (`id`),
   FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   FOREIGN KEY (`post_id`) REFERENCES `post` (`id`)
